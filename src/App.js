554ca104a4336a1dc95ed1ee32d6870c6d50d855
sarahkirby <sk.irby@icloud.com>
2016-03-18 11:43:34 +1300

    creating & rending app, exporting & importing a child component, passing data into components, iterating over data with jsx.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
  import React from 'react';
+ import ContactsList from './ContactsList';
+
+ let contacts = [{
+ 	id: 1,
+ 	name: 'Sarah',
+ 	phone: '555 555 5555'
+ }, {
+ 	id: 2,
+ 	name: 'Cat',
+ 	phone: '123 555 5555'
+ }, {
+ 	id: 3,
+ 	name: 'Ben',
+ 	phone: '456 555 5555'
+ }, {
+ 	id: 4,
+ 	name: 'Someone',
+ 	phone: '789 555 5555'
+ }]
 
  class App extends React.Component {
  	render() {
  		return (
  			<div>
  				<h1>Contacts List</h1>
+ 				<ContactsList contacts={this.props.contacts} />
  			</div>
 		)
  	}
  }
 
- React.render(<App />, document.getElementById('app'));
+ React.render(<App contacts={contacts}/>, document.getElementById('app'));
