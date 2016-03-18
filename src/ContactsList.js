01f4c8d4710b6652cee1952f94e09411094f50cd
Sarah Kirby <sk.irby@icloud.com>
2016-03-18 14:46:36 +1300

    covered controller inputs and building a search filter.

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -2,13 +2,32 @@
  import Contact from './Contact';
 
  class ContactsList extends React.Component {
+ 	constructor() {
+ 		super();
+ 		this.state = {
+ 			search: ''
+ 		};
+ 	}
+
+ 	updateSearch(event) {
+ 		this.setState({search: event.target.value.substr(0,20)});
+ 	}
+
  	render() {
+ 		let filteredContacts = this.props.contacts.filter(
+ 			(contact) => {
+ 				return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+ 			}
+		);
  		return (
+ 			<div>
+ 				<input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)} />
  				<ul>
- 					{this.props.contacts.map((contact)=> {
+ 					{filteredContacts.map((contact)=> {
  						return <Contact contact={contact} key={contact.id} />
  					})}
  				</ul>
+			</div>
 		)
  	}
  }
